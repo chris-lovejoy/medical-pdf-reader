29a276dfc665ad6d1382915bf9fbefd16a5e6d8b
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2023-06-22 15:54:39 +0100

    handle asynchronicity in testing via asyncio

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,4 +1,5 @@
 import pytest
+import asyncio
 from src.parse.parse_to_clinical import TextToClinicalJSON
 
 sample_text = "This can become a sample clinical record, which needs to be parsed into the specified clinical format."
@@ -16,7 +17,7 @@ def test_parse_text_to_clinical_json():
     Tests that parsing and cleaning of the text is performed using the TextToClinicalJSON parser class
     """
     parser = TextToClinicalJSON(sample_text)
-    parser.parse_text_to_clinical_json()
+    asyncio.run(parser.parse_text_to_clinical_json())
     assert parser.clinical_json is not None 
     # TODO: update to a definition of reasonably accurate parsing
     # (Will need to incorporate the structure specified in the pydantic data model)
