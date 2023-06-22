0abcd2ef7a956347090948a55cae42056ee64037
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2023-06-22 15:16:48 +0100

    add load of initial text

--- src/extract/extract_and_clean_pdf.py
+++ src/extract/extract_and_clean_pdf.py
@@ -1,6 +1,7 @@
 """This file extracts and cleans text from the medical record PDF"""
 
 
+from unstructured.partition.pdf import partition_pdf
 
 class PDFtoText:
     """
@@ -11,3 +12,15 @@ class PDFtoText:
         self.dir = pdf_dir
         self.initial_text = None
         
+    
+    def load_initial_text(self):
+        """
+        This function loads the initial text from the PDF using the unstructured library.
+        """
+        elements = partition_pdf(self.dir, ocr_languages="eng", include_page_breaks=False)
+        text = ""
+        for element in elements:
+            text += "\n" + str(element)
+        self.initial_text = text
+
+    
