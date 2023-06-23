60c7ace35a81e1d74c53376868454eb6afe292eb
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2023-06-23 16:18:42 +0100

    add query handling, source quote spec, evaluation confidence

--- src/query/query.py
+++ src/query/query.py
@@ -1,6 +1,7 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
+from langchain import LLMChain
 
 from .. import prompts
 from .. import config
@@ -21,7 +22,7 @@ class QueryClinicalJSON():
         self.medical_record_extracts = self.extract_strings_from_clinical_json()
         self.vectorstore = FAISS.from_texts(self.medical_record_extracts, OpenAIEmbeddings())
         self.extracted_responses = {}
-
+        self.query_responses = []
 
     def extract_info(self, info_to_extract):
         """
@@ -54,7 +55,90 @@ class QueryClinicalJSON():
         # Save extracted response in a dictionary
         self.extracted_responses[info_to_extract] = result['result']
 
+    def answer_query(self, query):
+        """
+        This function uses retrieval-augmented question-answering to answer
+        a query (as True/False). It uses the specify_source_quote and evaluate_confidence
+        functions to also identify the precise relevant part of the medical
+        record and a confidence score for the answer.
+        """
 
+        if self.query_chain_type == "map_reduce":            
+            chain_type_kwargs = {"question_prompt": prompts.answer_query_map_prompt,
+                    "combine_prompt": prompts.answer_query_combine_prompt}
+        elif self.query_chain_type == "stuff":
+            chain_type_kwargs = {"prompt": prompts.answer_query_stuff_prompt}
+        else:
+            chain_type_kwargs = {}
+
+        # Define question-answering chain for query
+        qa = RetrievalQA.from_chain_type(
+            llm=self.answer_query_llm,
+            chain_type=self.query_chain_type, 
+            retriever=self.vectorstore.as_retriever(search_kwargs=dict(k=3)),
+            chain_type_kwargs=chain_type_kwargs,
+            return_source_documents=True
+            # verbose=True,
+        )
+
+        # Get result from query
+        result = qa({"query": query})
+        answer = result['result']
+        answer = self.enforce_true_false(answer)
+
+        # Extract precise source quote
+        source_docs = result['source_documents']
+        source_quote = self.specify_source_quote(query, answer, source_docs)
+
+        # Get confidence score
+        confidence_score = self.evaluate_confidence(query, answer, source_quote)
+
+        # Define dictionary entry and append to query responses
+        query_response_dict = {
+            "query": query,
+            "answer": answer,
+            "source_quote": source_quote,
+            "confidence_score": confidence_score
+        }
+        self.query_responses.append(query_response_dict)
+
+    def specify_source_quote(self, query, answer, source_docs):
+        """
+        Identifies the specific quoted source from the patient record which
+        supports the answer to the query.
+        NOTE: In future, could look to map this to source content more explicitly.
+        """
+
+        # Join source content across all of the source docs used by the retrieval QA chain
+        source_content = "\n".join([doc.page_content for doc in source_docs])
+
+        # Specify and execute chain
+        llm_chain = LLMChain(
+            llm=self.specify_source_llm,
+            prompt=prompts.specify_source_prompt
+        )
+        source_quote = llm_chain.apply([
+            {
+            "query": query,
+            "answer": answer,
+            "source_content": source_content
+            },
+        ])
+    
+        return source_quote[0]['text']
+
+    def enforce_true_false(self, answer):
+        """
+        This function looks for 'true' or 'false' in the LLM response 
+        text and converts it into a boolean True or False. This helps
+        to handle LLM variations such as "True." or "True, because..."
+        """
+        if "true" in answer.lower():
+            return True
+        elif "false" in answer.lower():
+            return False
+        else:
+            return None # TODO: test that None is handled okay. If not use "" or alternative
 
     def extract_strings_from_clinical_json(self):
         """
