0a522baffac80394f774aa6d3e420b2aabc6e2e1
Chris Lovejoy <christopher.lovejoy1@gmail.com>
2023-06-24 22:17:54 +0100

    initial eval object

--- src/reasoning/clinical_eval.py
+++ src/reasoning/clinical_eval.py
@@ -1,1 +1,13 @@
-# TODO: implement simple prompt here
+
+class ClinicalEval():
+    """
+    This object can be used to evaluate the whether a clinical decision
+    is appropriate.
+    """
+    def __init__(self, clinical_json, query_responses):
+
+        # Load required variables
+        self.clinical_json = clinical_json
+        self.query_responses = query_responses
+        
+
